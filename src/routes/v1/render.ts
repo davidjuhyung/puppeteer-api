76f09e54893b9a356b68a25fc15fb09b792ce00d
David Kim <j626kim@uwaterloo.ca>
2022-04-20 07:42:28 -0400

    added / route

--- src/routes/v1/render.ts
+++ src/routes/v1/render.ts
@@ -2,7 +2,15 @@ import restify, { Server } from "restify"
 import { renderHTML } from "../../handlers/v1/render"
 
 export const initRenderRoutes = (server: Server) => {
-  server.post('/render', restify.plugins.conditionalHandler([
+  server.get('/render', restify.plugins.conditionalHandler([
     { version: '1.0.0', handler: renderHTML },
   ]))
+
+  server.get('/', restify.plugins.conditionalHandler([
+    { version: '1.0.0', handler: (req, res, next) => {
+      res.send('Hello world')
+      next()
+    } },
+  ]))
+
 }
